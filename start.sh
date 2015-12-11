7739ae6e8b6a426c334d8b2b9bc09e203a6a5063
Matthew M. Boedicker <matthewm@boedicker.org>
2015-12-10 20:44:17 -0800

    Remove extra local

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ export DISPLAY=:0
 ADDRS=$(tr -d '\n' < urls)
 
 for LOCAL in $(ls local/*.html); do
-  ADDRS="$ADDRS http://localhost:8000/local/$LOCAL"
+  ADDRS="$ADDRS http://localhost:8000/$LOCAL"
 done
 
 echo $ADDRS
